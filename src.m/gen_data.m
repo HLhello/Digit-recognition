9ea5dc49debc1f9ba6a34333d39643e6cb387deb
HL_1995 <347142093@qq.com>
2020-08-14 19:36:13 +0800

    update work log

--- src.m/gen_data.m
+++ src.m/gen_data.m
@@ -6,8 +6,10 @@ dd = fscanf(f1,'%x');
 figdata = dec2hex(dd);
 f2 = fopen('figdata.txt','w');
 for ii = 1:1:length(figdata)
-    fprintf(f2, '%s%s\n', figdata(ii,3),figdata(ii,4));
-    fprintf(f2, '%s%s\n', figdata(ii,1),figdata(ii,2));
+    fprintf(f2, '%s\n', figdata(ii,4));
+    fprintf(f2, '%s\n', figdata(ii,3));
+    fprintf(f2, '%s\n', figdata(ii,2));
+    fprintf(f2, '%s\n', figdata(ii,1));
 end
 fclose(f1);
 fclose(f2);
