ee9e3e6e8f533333f0ae35b5055c4b3f17461976
HL_1995 <347142093@qq.com>
2020-08-14 11:23:11 +0800

    update work log

--- src.m/gen_data.m
+++ src.m/gen_data.m
@@ -1,10 +1,13 @@
 clc
 clear
 close all 
-
-figdata=textread('logo.txt','%s');
-fp = fopen('figdata.txt','w');
+f1 = fopen('logo.txt', 'r');
+dd = fscanf(f1,'%x');
+figdata = dec2hex(dd);
+f2 = fopen('figdata.txt','w');
 for ii = 1:1:length(figdata)
-    fprintf(fp, '%s\n', figdata(ii));
+    fprintf(f2, '%s%s\n', figdata(ii,3),figdata(ii,4));
+    fprintf(f2, '%s%s\n', figdata(ii,1),figdata(ii,2));
 end
-fclose(fp);
+fclose(f1);
+fclose(f2);
